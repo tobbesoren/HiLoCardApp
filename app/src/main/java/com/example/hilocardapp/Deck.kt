f4af822b5fa3a5bdfdac18a12ff19e8ba797d544
Tobias Sörensson <tobbesoren@gmail.com>
2022-11-21 11:58:21 +0100

    Added comments

--- app/src/main/java/com/example/hilocardapp/Deck.kt
+++ app/src/main/java/com/example/hilocardapp/Deck.kt
@@ -1,41 +1,64 @@
 package com.example.hilocardapp
 
 /*
-A Deck can hold an unlimited number of cards
+A Deck can hold an unlimited number of cards in two mutable lists;
+mainDeck and discardedCards.
+Has functions to draw a card, shuffle deck and add card/s between mutable lists.
  */
 open class Deck {
     var mainDeck = mutableListOf<Card>()
     var discardedCards = mutableListOf<Card>()
 
+    /*
+    Moves discardedCards to mainDeck and shuffles the list.
+     */
     fun shuffleDeck() {
-        for(card in discardedCards) {
-            mainDeck.add(card)
-        }
+        addCardList(discardedCards, mainDeck)
         discardedCards.clear()
         mainDeck.shuffle()
     }
 
+    /*
+    Used to draw a card. It moves the next card from mainDeck to disCardedCards.
+    If mainDeck is empty, it calls shuffleDeck() first. It returns the drawn card.
+     */
     fun drawCard() : Card {
-        if(mainDeck.size > 0) {
-            discardedCards.add(mainDeck[0])
-            mainDeck.removeAt(0)
-            return discardedCards.last()
+        if(mainDeck.size == 0) {
+            shuffleDeck()
         }
-        discardedCards.add(mainDeck[0]) // This is bonkers and needs to be changed!
+
+        addCard(mainDeck[0], discardedCards)
         mainDeck.removeAt(0)
         return discardedCards.last()
     }
 
-    fun addCard(card: Card) {
-        mainDeck.add(card)
+    /*
+    Used to add a card to a mutableList of cards
+     */
+    fun addCard(card: Card, cardListToAddTo: MutableList<Card>) {
+        cardListToAddTo.add(card)
     }
 
-    fun addDeck(deck: Deck) {
-        for(card in deck.mainDeck) {
-            addCard(card)
+    /*
+    Used to add a mutableList of cards to another mutableList of cards
+     */
+    fun addCardList(cardsToAdd: MutableList<Card>, cardListToAddTo: MutableList<Card>) {
+        for(card in cardsToAdd) {
+            addCard(card, cardListToAddTo)
         }
     }
 
+    /*
+    Used to add a deck.mainDeck to this deck's mainDeck.
+    Not used for the moment; may be used to play with multiple decks.
+     */
+    fun addDeck(deck: Deck) {
+        addCardList(deck.mainDeck, mainDeck)
+    }
+
+    /*
+    Only used for development
+     */
     fun logDeck() {
         for(card in mainDeck) {
             card.logCard()
